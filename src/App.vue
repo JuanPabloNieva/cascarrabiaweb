3185d78cf21b7db0ed91c56869a3294094757ac1
JuanPabloNieva <juanpablonieva@outlook.com>
2021-04-15 20:04:18 -0300

    Preloader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="loader" class="fondo-cargando">
-      <img class="cargando" src="@/assets/Logo-circular.png">
-      <p class="text-cargando">cargando...</p>
-    </div>
+    <loader/>
     <navegador/>
     <nav-extendido/>
     <router-view/>
@@ -11,18 +8,12 @@
 </template>
 
 <script>
+import Loader from './components/Loader.vue'
 import Navegador from './components/Navegador.vue'
 import NavExtendido from './components/NavExtendido.vue'
 
 export default {
-  components: { Navegador, NavExtendido },
-  mounted: function () {
-    const div = document.getElementById('loader')
-    const el = window.getComputedStyle(div)
-    if (el.getPropertyValue('display') === 'flex') {
-      div.style.display = 'none'
-    }
-  }
+  components: { Navegador, NavExtendido, Loader }
 }
 </script>
 
@@ -72,44 +63,6 @@ html {
   margin: auto;
   padding: 10px;
   width: 25vw;
-}
-
-/* Cargando pagina*/
-
-.fondo-cargando {
-  display: flex;
-  position: absolute;
-  z-index: 10;
-  flex-direction: column;
-  justify-items: start;
-  width: 100vw;
-  height: 100vh;
-  background-color: var(--fondo-transparente);
-}
-
-.text-cargando {
-  margin: 50px auto;
-  font-size: 30px;
-  font-weight: bold;
-  color:var(--link-focus-color)
-}
-
-.cargando {
-  margin: 150px auto;
-  width: 100px;
-  height: 100px;
-  -webkit-animation: spin 2s linear infinite; /* Safari */
-  animation: spin 1.5s linear infinite;
-}
-
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
 }
 
 @media (prefers-color-scheme:dark){
---
+++ src/components/Loader.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="loader" class="fondo-cargando" v-if="!isloaded">
+      <img class="cargando" src="@/assets/Logo-circular.png">
+      <p class="text-cargando">cargando...</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isloaded: false
+    }
+  },
+  mounted () {
+    document.onreadystatechange = () => {
+      if (document.readyState === 'complete') {
+        this.isloaded = true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+/* Cargando pagina*/
+
+.fondo-cargando {
+  display: flex;
+  position: absolute;
+  z-index: 10;
+  flex-direction: column;
+  justify-items: start;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--fondo-transparente);
+}
+
+.text-cargando {
+  margin: 50px auto;
+  font-size: 30px;
+  font-weight: bold;
+  color:var(--link-focus-color)
+}
+
+.cargando {
+  margin: 150px auto;
+  width: 100px;
+  height: 100px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 1.5s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
